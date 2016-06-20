0896bd88a75e761a745e6ec79ce84a005bce0359
jihoonpark <jjjhhhvvv@gmail.com>
2016-06-20 11:38:47 +0900

    spring mybatis 설정파일 변경 및   api 추가   -jdbc  dbcp

---
+++ HwangDangFleamarket/test.sql
@@ -0,0 +1,18 @@
+
+
+CREATE TABLE  memberTest (
+
+	member_id varchar2(30) primary key ,
+	member_password  varchar2(20)  NOT NULL,
+	member_name  varchar2(18)  NOT NULL,
+	member_phone  char(18)  NOT NULL,
+	member_zipcode  varchar2(18)  NOT NULL,
+	member_address  varchar2(18)  NOT NULL,
+	member_sub_address  varchar2(18)  NOT NULL,
+	member_assign  number(1)  NOT NULL
+)
+
+	CREATE TABLE seller (
+	
+	
+	)
